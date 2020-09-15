4c4f9769f94a58c3fb1adfb77693d441599b0d75
ashish <ashishmangla1991@gmail.com>
2020-09-15 11:23:21 +0530

    add sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Sidebar from './Sidebar';
 const Layout = ({children}) => {
     return (
         <>
-            <Header />
+        <Sidebar/>
+        
+        <aside className="aside-right">
+        <Header />
             <main>
                 {children}
             </main>
             <Footer />
+        </aside>
+
+            
                
         </>
     )
---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Sidebar = () =>{
+return (
+    <>
+    <aside className="aside-left">
+    this is sidebar
+    </aside>
+      
+    </>
+)
+}
+
+export default Sidebar
