b8114360e4ddf83afd395da11b39700e39637418
ashish <ashishmangla1991@gmail.com>
2020-09-15 10:59:48 +0530

    inital commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header  = () =>{
+    return (
+        <>
+            this is Header
+        </>
+    )
+}
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+const Index  = () =>{
+return(
+    <>
+        <Layout>
+            this is index page
+        </Layout>
+    </>
+)
+}
+
+
+export default Index
